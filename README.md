51c695c22367731c3af51af4e8b76b4bdbb1a8eb
Solomon Sunday <solomon@Solomons-MBP-2.wp.shawcable.net>
2023-08-12 19:03:12 -0500

    pushing the local ecommerce store to my repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# ViewKart
+# EcomStore
---
+++ carts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from store.models import Product, Variation
+from accounts.models import Account
+
+# Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+    
+
+class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+
+
+
+    def __unicode__(self):
+        return self.product

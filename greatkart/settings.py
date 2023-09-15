f11bde27eb54912a748a693635c2d04dc69d6ee3
Solomon Sunday <ssdahymond@gmail.com>
2023-09-15 17:57:14 -0500

    updated gitignore and remove sensitive file from settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret import SMTP_pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -152,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ssdahymond@gmail.com'
-EMAIL_HOST_PASSWORD = 'pbiwmkqiyzdyvxpi'
+EMAIL_HOST_PASSWORD = SMTP_pass
 EMAIL_USE_TLS = True

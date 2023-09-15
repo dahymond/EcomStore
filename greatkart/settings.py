13e9ecbf3616c37a97e84380391fb3003fbd2ec9
Solomon Sunday <ssdahymond@gmail.com>
2023-09-15 15:53:54 -0500

    conpletd the checkout page

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ssdahymond@gmail.com'
-EMAIL_HOST_PASSWORD = 'kgbbrutljkxdoodz'
+EMAIL_HOST_PASSWORD = 'pbiwmkqiyzdyvxpi'
 EMAIL_USE_TLS = True

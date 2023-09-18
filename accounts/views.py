a56f9c0615e38eb6417b0580451fc93dd9874392
Solomon Sunday <ssdahymond@gmail.com>
2023-09-18 15:12:23 -0500

    updated the group variation in the cart

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,9 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -58,7 +61,21 @@ def login(request):
         password = request.POST.get('password')
 
         user = auth.authenticate(email=email, password=password)
+        
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            
+            except:
+                pass
+            
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
             return redirect('home')

f3f3c63fd43f3a455bb1030dbb3eaff27dc79837
Solomon Sunday <ssdahymond@gmail.com>
2023-09-23 13:47:30 -0500

    updated repo to resolve the repo

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
-from carts.models import Cart, CartItem
+from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-# Create your views here
+# Create your views here.
+from django.http import HttpResponse
+
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
@@ -129,6 +130,37 @@ def add_cart(request, product_id):
             cart_item.save()
         return redirect('cart')
 
+
+def remove_cart(request, product_id, cart_item_id):
+
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id, cart_item_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.user.is_authenticated:
+        cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+    else:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+    cart_item.delete()
+    return redirect('cart')
+
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
@@ -141,68 +173,44 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        
         tax = (2 * total)/100
         grand_total = total + tax
-
     except ObjectDoesNotExist:
         pass #just ignore
 
-
     context = {
-        'total': total, 
-        'quantity': quantity, 
+        'total': total,
+        'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax'       : tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
+
 
 @login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
         grand_total = 0
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        
         tax = (2 * total)/100
         grand_total = total + tax
-
     except ObjectDoesNotExist:
         pass #just ignore
 
-
     context = {
-        'total': total, 
-        'quantity': quantity, 
+        'total': total,
+        'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax'       : tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/checkout.html', context)
-    
-def remove_cart(request, product_id, cart_item_id):
-    #product = Product.objects.get(id=product_id)
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Product, id=product_id)
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
-        if cart_item.quantity > 1:
-            cart_item.quantity -= 1
-            cart_item.save() 
-        else:
-            cart_item.delete()
-    except:
-        pass
-    return redirect('cart')
-
-def remove_cart_item(request, product_id, cart_item_id):
-    product = get_object_or_404(Product, id=product_id)
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    cart_items = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
-    cart_items.delete()
-    return redirect('cart')

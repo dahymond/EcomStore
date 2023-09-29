0ebd1f97aaa3a7983d4b884dd9f29c07c1f2f7ab
Solomon Sunday <ssdahymond@gmail.com>
2023-09-29 00:04:52 -0500

    completed dynamic page switching

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+import pip._vendor.requests as requests
 
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
@@ -105,7 +106,18 @@ def login(request):
             
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('home')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                
+                # next=cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+                return redirect('dashboard')
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'invalid login credentials')
             return redirect('login')
